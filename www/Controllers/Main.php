313ba0933d406ae01a52895f82a2468902061ca6
Arezki <114149869+ArezkiBe@users.noreply.github.com>
2023-10-20 12:19:43 +0200

    maj

---
+++ www/Controllers/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+class Main{
+
+    public function home(): void
+    {
+        echo "Page d'accueil";
+    }
+
+    public function aboutUs(): void
+    {
+        echo "ceci est la page a propos";
+    }
+
+}
