48cbfe5fb395edc6e571c7bb5f10fcfc7cfb69d5
AMARA Bilal <amara.bilalpro@gmail.com>
2024-02-19 14:44:55 +0100

    Ajout des view home-contact-aPropos

---
+++ www/Views/Design/apropos.view.php
@@ -0,0 +1,12 @@
+<section>
+     <h2>Notre Histoire</h2>
+     <p>Brief description of your restaurant's history...</p>
+</section>
+<section>
+     <h2>Notre Équipe</h2>
+     <p>Description of your team members...</p>
+</section>
+<section>
+     <h2>Notre Cuisine</h2>
+     <p>Description of your restaurant's cuisine...</p>
+</section>
---
+++ www/Views/Design/contact.view.php
@@ -0,0 +1,24 @@
+<section>
+     <h2>Pour Nous Suivre</h2>
+     <p>Suivez-nous sur les réseaux sociaux pour rester informé des dernières nouvelles, des offres spéciales et des événements à venir.</p>
+     <ul>
+          <li><a href="https://www.facebook.com/votrerestaurant" target="_blank">Facebook</a></li>
+          <li><a href="https://twitter.com/votrerestaurant" target="_blank">Twitter</a></li>
+          <li><a href="https://www.instagram.com/votrerestaurant" target="_blank">Instagram</a></li>
+     </ul>
+</section>
+<section>
+     <h2>Coordonnées</h2>
+     <p>123 Rue Principale<br>Ville, Pays<br>Tél: +123 456 789<br>Email: info@votrerestaurant.com</p>
+</section>
+     <h2>Formulaire de Contact</h2>
+     <form action="/submit_form.php" method="post">
+          <label for="name">Nom:</label><br>
+          <input type="text" id="name" name="name"><br>
+          <label for="email">Email:</label><br>
+          <input type="email" id="email" name="email"><br>
+          <label for="message">Message:</label><br>
+          <textarea id="message" name="message" rows="4" cols="50"></textarea><br>
+          <input type="submit" value="Envoyer">
+     </form>
+</section>
