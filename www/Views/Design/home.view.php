d94baf87ed4f9a6d2ff0876a6db577d393f11e60
AMARA Bilal <amara.bilalpro@gmail.com>
2024-02-19 14:44:55 +0100

    Ajout des view home-contact-aPropos

---
+++ www/Views/Design/home.view.php
@@ -0,0 +1,101 @@
+<section>
+	<nav class="navbar">
+		<div class="container">
+               <a href="#" class="navbar_title">Navbar</a>
+               <button class="navbar_toggle_button" data-target="#content">
+                    menu
+               </button>
+               <div class="navbar_toggle_content" id="content">
+                    <ul>
+                         <li>
+                              <a href="#" class="active">Home</a>
+                         </li>
+                         <li>
+                              <a href="#">Features</a>
+                         </li>
+                         <li>
+                              <a href="#">Pricing</a>
+                         </li>
+                         <li>
+                              <a href="#" class="disabled">Disabled</a>
+                         </li>
+                    </ul>
+                    <form>
+                         <input type="text" placeholder="Search" />
+                         <button class="button button-outline">Search</button>
+                    </form>
+               </div>
+	     </div>
+	</nav>
+</section>
+<section>
+	<div
+		class="banner banner-text"
+		style="background-image: url('/images/fond.jpg')">
+		<p id="editable-paragraph">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+			ullamcorper nulla a ipsum imperdiet, ac porttitor dui sagittis.
+		</p>
+	</div>
+</section>
+<section>
+	<h1>Menu</h1>
+	<hr />
+	<div class="flex-container">
+		<div class="flex-item">
+			<article class="card">
+				<img src="/images/totoro.jpg" />
+				<h1>Card title</h1>
+				<p>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+				</p>
+				<a href="#" class="button button-primary button-sm">Button</a>
+			</article>
+		</div>
+          <div class="flex-item">
+			<article class="card">
+				<img src="/images/totoro.jpg" />
+				<h1>Card title</h1>
+				<p>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+				</p>
+				<a href="#" class="button button-primary button-sm">Button</a>
+			</article>
+		</div>
+          <div class="flex-item">
+			<article class="card">
+				<img src="/images/totoro.jpg" />
+				<h1>Card title</h1>
+				<p>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+				</p>
+				<a href="#" class="button button-primary button-sm">Button</a>
+			</article>
+		</div>	
+	</div>
+</section>	
+<section>
+    <div class="container">
+          <h2>Horaires et Coordonnées</h2>
+          <p><strong>Horaires d'ouverture :</strong> Lundi - Dimanche : 11h00 - 22h00</p>
+          <p><strong>Adresse :</strong> 123 Rue du Restaurant, Ville, Code Postal</p>
+          <p><strong>Téléphone :</strong> +XX XXX XXX XXX</p>
+          <p><strong>Email :</strong> info@restaurant.com</p>
+    </div>
+</section>
+<section>
+<section>
+     <h1>Galerie de Photos</h1>
+	<hr />
+	<div class="slider" data-options="{}">
+		<img src="/images/image1.jpg" />
+		<img src="/images/image2.jpg" />
+		<img src="/images/image3.jpg" />
+		<img src="/images/image4.jpg" />
+		<img src="/images/image5.jpg" />
+		<img src="/images/image6.jpg" />
+	</div>
+</section>
