253f1cbf2258821d0f55c599e29e68e31828c3cc
AMARA Bilal <amara.bilalpro@gmail.com>
2024-02-19 15:12:27 +0100

    modification routes.yaml pour les nouvelles view

--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -2,6 +2,10 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<link rel="stylesheet" type="text/css" href="dist/css/style.css" />
+		<script src="dist/js/main.js"></script>
         <title>Front</title>
     </head>
     <body>
