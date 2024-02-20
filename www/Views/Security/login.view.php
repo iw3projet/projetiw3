391332b686a355b92c52d13022ccd9b73a72a624
Dany Junior SAMBO LEBINDZI <pstation242@gmail.com>
2024-02-20 09:45:17 +0100

    ajout du css part2 et integration

--- www/Views/Security/login.view.php
+++ www/Views/Security/login.view.php
@@ -3,6 +3,7 @@
         <span class="material-symbols-outlined logoFormulaire">person</span>
         <h2>Connexion</h2>
         <?php $this->modal("form", $form) ?>
+        <p>Vous n’avez pas de compte ? <a href="InscriptionPage.php" class="creerCompte">Créer un compte</a></p>
     </form>
 </div>
 
