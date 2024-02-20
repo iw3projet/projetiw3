03dfe87c54c16f125a43242c58f65da1b1f40401
Dany Junior SAMBO LEBINDZI <pstation242@gmail.com>
2024-02-20 11:55:45 +0100

    add integration css part3

--- www/Views/Security/login.view.php
+++ www/Views/Security/login.view.php
@@ -3,6 +3,6 @@
         <span class="material-symbols-outlined">person</span>
         <h2>Connexion</h2>
         <?php $this->modal("form", $form) ?>
-        <p>Vous n’avez pas de compte ? <a href="InscriptionPage.php" class="creerCompte">Créer un compte</a></p>
+        <p class="creerCompte">Vous n’avez pas de compte ? <a href="InscriptionPage.php">Créer un compte</a></p>
     </form>
 </div>
