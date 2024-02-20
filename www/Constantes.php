36567bfd1c37c17f148dd4f3cc56791dac302514
ArezkiBe <abeggar@myges.fr>
2024-02-20 21:59:47 +0100

    suppression dossier constantes.php

--- www/Constantes.php
+++
@@ -1,6 +0,0 @@
-<?php
-                define("DB_HOST", "postgres");
-                define("DB_NAME", "esgi");
-                define("DB_USERNAME", "esgi");
-                define("DB_PWD", "esgipwd");
-            define("PREFIX", "a998");
