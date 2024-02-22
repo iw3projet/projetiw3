1d68203547b0a83a971b01ad603045b3e8dd0f21
ArezkiBe <abeggar@myges.fr>
2024-02-22 14:26:22 +0100

    debug

--- www/Views/Templates/back.tpl.php
+++ www/Views/Templates/back.tpl.php
@@ -14,14 +14,10 @@
         <div class="containerBack">
             <nav class="navigation">
                 <p class="leftSide button button-outline"><a href="/prebuild" class="dashboard-text">Creation</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="article-text">Articles</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="media-text">Medias</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="page-text">Pages</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="comment-text">Comments</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="appearance-text">Appearance</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="settings-text">Settings</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="user-text">Users</a></p>
-                <p class="leftSide button button-outline"><a href="#" class="tools-text">Tools</a></p>
+                <p class="leftSide button button-outline"><a href="/showallpages" class="page-text">Pages</a></p>
+                <p class="leftSide button button-outline"><a href="/addreview" class="comment-text">Comments</a></p>
+                <p class="leftSide button button-outline"><a href="/update-user" class="user-text">User</a></p>
+                <p class="leftSide button button-outline"><a href="/role" class="user-text">Role</a></p>
             </nav>
             <main class="section">
                 <?php
--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -1,45 +1,9 @@
-<!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <title><?php echo ($this->data["title"])?></title>
-        <meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width,initial-scale=1" />
-		<link rel="stylesheet" type="text/css" href="dist/css/style.css" />
-		<script src="dist/js/main.js"></script>
-        <title>Front</title>
     </head>
     <body>
-    <h1>Template du front</h1>
-    <nav class="navbar">
-		<div class="container">
-               <a href="#" class="navbar_title">Navbar</a>
-               <button class="navbar_toggle_button" data-target="#content">
-                    menu
-               </button>
-               <div class="navbar_toggle_content" id="content">
-                    <ul>
-                         <li>
-                              <a href="#" class="active">Home</a>
-                         </li>
-                         <li>
-                              <a href="#">Features</a>
-                         </li>
-                         <li>
-                              <a href="#">Pricing</a>
-                         </li>
-                         <li>
-                              <a href="#" class="disabled">Disabled</a>
-                         </li>
-                    </ul>
-                    <form>
-                         <input type="text" placeholder="Search" />
-                         <button class="button button-outline">Search</button>
-                    </form>
-               </div>
-	     </div>
-	</nav>
-        
         <?php include $this->view;?>
     </body>
 </html>
