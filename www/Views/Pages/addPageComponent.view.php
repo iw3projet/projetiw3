126f15d8c0c828e7076e7c14141a5aab6e8039c5
Dany Junior SAMBO LEBINDZI <pstation242@gmail.com>
2024-02-22 05:35:06 +0100

    Add template integration

--- www/Views/Pages/addPageComponent.view.php
+++ www/Views/Pages/addPageComponent.view.php
@@ -1,9 +1,9 @@
-<h1>AddPage</h1>
-<?php $this->modal("form", $form,$formErrors)?>
-
-<iframe src="/previewpage?preview=<?php echo($_GET["tpl"])?>" frameborder="0"></iframe>
-
-
-
-        
-
+<div class="wizwig">
+    <div class="left-section">
+        <h1>AddPage</h1>
+        <?php $this->modal("form", $form,$formErrors)?>
+    </div>
+    <div class="right-section">
+        <iframe src="/previewpage?preview=<?php echo($_GET["tpl"])?>" frameborder="0"></iframe>
+    </div>
+</div>
