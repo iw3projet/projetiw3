27ae01b152b178c04c11bc8b988d350744b0c86c
AMARA Bilal <amara.bilalpro@gmail.com>
2024-02-22 11:44:09 +0100

    test OK review

--- www/esgi.sql
+++ www/esgi.sql
@@ -18,63 +18,20 @@ CREATE TABLE iciprefix_page (
     content TEXT,
     slug  VARCHAR(50) UNIQUE,
     template VARCHAR(100),
+    user_id integer,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER REFERENCES iciprefix_user(id)
+    FOREIGN KEY (user_id) REFERENCES iciprefix_user(id)
 );
 
-CREATE TABLE iciprefix_comment (
-    id SERIAL,
+CREATE TABLE iciprefix_review (
+    id SERIAL PRIMARY KEY,
     content VARCHAR(255),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     approved smallint,
     user_id integer,
-    page_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (page_id) REFERENCES iciprefix_page(id),
     FOREIGN KEY (user_id) REFERENCES iciprefix_user(id)
-);
-
-CREATE TABLE iciprefix_media (
-    id SERIAL,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    page_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (page_id) REFERENCES iciprefix_page(id)
-);
-
-CREATE TABLE iciprefix_setting (
-    id SERIAL,
-    name VARCHAR(255),
-    value VARCHAR(45),
-    autoload smallint,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-);
-
-
-
-CREATE TABLE iciprefix_template (
-    id SERIAL,
-    name VARCHAR(255),
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    page_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (page_id) REFERENCES iciprefix_page(id)
-);
-
-
-
-CREATE TABLE iciprefix_term (
-    id SERIAL,
-    tagname VARCHAR(255),
-    page_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (page_id) REFERENCES iciprefix_page(id)
 );
 
 CREATE TABLE iciprefix_passwordReset (
@@ -87,41 +44,10 @@ CREATE TABLE iciprefix_passwordReset (
 
 CREATE TABLE iciprefix_pages (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  layout VARCHAR(255) NOT NULL,
-  data TEXT,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE iciprefix_page_translations (
-  id SERIAL PRIMARY KEY,
-  page_id INT NOT NULL,
-  locale VARCHAR(50) NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  meta_title VARCHAR(255) NOT NULL,
-  meta_description VARCHAR(255) NOT NULL,
-  route VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  approved integer,
+  user_id integer,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  UNIQUE (page_id, locale),
-  FOREIGN KEY (page_id) REFERENCES iciprefix_pages(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE iciprefix_uploads (
-  id SERIAL PRIMARY KEY,
-  public_id VARCHAR(50) NOT NULL,
-  original_file VARCHAR(512) NOT NULL,
-  mime_type VARCHAR(50) NOT NULL,
-  server_file VARCHAR(512) NOT NULL,
-  UNIQUE (public_id),
-  UNIQUE (server_file)
-);
-
-CREATE TABLE iciprefix_settings (
-  id SERIAL PRIMARY KEY,
-  setting VARCHAR(50) NOT NULL,
-  value TEXT NOT NULL,
-  is_array BOOLEAN NOT NULL,
-  UNIQUE (setting)
+  FOREIGN KEY (user_id) REFERENCES iciprefix_user(id)
 );
